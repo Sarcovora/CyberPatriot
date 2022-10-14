5e1f6e08d49988e871ffeb93d52021adca115290
Robby <robby@teals.org>
2022-10-14 00:30:35 -0500

    additional features

--- UbuntuScript.sh
+++ UbuntuScript.sh
@@ -7,7 +7,7 @@ if [ $UID != '0' ]
     exit
 fi
 
-# fine operating system
+# find operating system
 if hostnamectl | grep "Operating System" | grep -i "Fedora"; then
 	echo "Fedora"
 else if hostnamectl | grep "Operating System" | grep -i "Ubuntu"; then
@@ -20,15 +20,31 @@ fi
 fi
 fi
 
-# install firewall
+# #update the system
+apt-get update -y
+apt-get upgrade -y
+
+# change all passwords
+for user in awk -F':' '{ print $1}' /etc/passwd
+do
+	echo -e "CyberPatriot123\!nCyberPatriot123\!" | passwd user
+done
+
+# enable firewall
 ufw enable
 
-# other stuff
+# configure password history
+chmod 777 /etc/logon.defs
+sed -i 's/PASS_MAX_DAYS.*/PASS_MAX_DAYS 90/g' /etc/logon.defs
+sed -i 's/PASS_MIN_DAYS.*/PASS_MIN_DAYS 10/g' /etc/logon.defs
+sed -i 's/PASS_WARN_AGE.*/PASS_WARN_AGE 7/g' /etc/logon.defs
+chmod 644 /etc/logon.defs
 
-# #update the system
-# apt-get update -y
-# apt-get upgrade -y
-
-
+# configure password requirements
 # #install ssh
 # apt-get install openssh-server -y
+# purge common unwanted services and malware
+# other stuff
+
+# list files in the user directory
+dir /home -R
